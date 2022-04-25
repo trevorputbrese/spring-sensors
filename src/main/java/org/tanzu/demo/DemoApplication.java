eb514bf7df177693beb42eb7028fbedf5aacd248
cpage-pivotal <cpage@pivotal.io>
2022-04-24 19:46:16 -0500

    Add HttpTraceRepository

--- src/main/java/org/tanzu/demo/DemoApplication.java
+++ src/main/java/org/tanzu/demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package org.tanzu.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +13,8 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
 }
